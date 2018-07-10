894836b3fd8eb90f8407feae68104903c3170a10
1512065 <38590756+1512065@users.noreply.github.com>
2018-07-10 13:22:48 +0700

    Delete Main.php

--- Ver2/Main.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('Db_connect.php');
-	include('Room.php');
-	$stmt = $conn->prepare("SELECT * from staff");
-	$stmt->setFetchMode(PDO::FETCH_CLASS.'Room');
-	$stmt->fetchAll();
-	
-	for each
-?>
