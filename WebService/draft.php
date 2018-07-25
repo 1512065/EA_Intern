c77608e0ad7ef4eb910354182ea48f1708d974f4
1512065 <1512065>
2018-07-25 10:44:10 +0700

    send mail via smtp

--- WebService/draft.php
+++ WebService/draft.php
@@ -1,5 +1,9 @@
 <?php
-	$str = "Khongco123";
+/*	$str = "Khongco123";
 	echo base64_encode($str);
 	echo chunk_split(base64_encode($str));
+	*/
+	$res='250 OK';
+	preg_match('/\d{3}\s.*/',$res,$matches);
+	print_r ($matches);
 ?>
