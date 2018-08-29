261b9a0bd8cae19ce885cc823b7b72821c3b4167
1512065 <1512065>
2018-08-29 09:19:58 +0700

    carousel fixed size

---
+++ Javascript/SlideShow/Carousel/script.js
@@ -0,0 +1,160 @@
+(function($){
+    $.fn.carousel = function(options) {
+        var setting = {
+            'width': 1000,
+            'height': 300,
+            'tranSpeed':1000,
+            'i_width': 320,
+            'i_height': 200,
+            'padding': 10
+        };
+
+        this.each(function() {
+            if(options) {
+                $.extend(setting, options);
+            }
+            // add class
+            $(this).children().wrapAll('<div class="carousel" />');
+           
+            var contain = $(this).find('.carousel');
+            
+            contain.find('li').wrapAll('<div class="slideFrame" />');
+            var slideFrame = contain.find('.slideFrame');
+
+            // add button next, prev
+            slideFrame.append('<div class="slideBtn"><a href="#" class="prev">&lt;</a> <a href="#" class="next">&gt;</a></div>');
+           
+            $('<div style="margin: 0 auto; width:'+ setting.width + 'px"><button class="play">AUTO</button> <button class="stop">STOP</button></div>')
+            .insertBefore(slideFrame);
+            $('li').wrap('<div class="itemFrame" />');
+            var itemFrame = contain.find('.itemFrame');
+
+            itemFrame.find('p').each(function() {
+                $(this).wrap('<div class="details" />');
+            });
+            itemFrame.find('img').each(function(){
+				// Caption
+				var title = $(this).attr('title');
+				$(this).wrapAll('<div class="image" />');
+				if (title !== undefined) {
+						$(this).attr('title', '');
+						$(this).after('<p>' + title + '</p>');
+				}
+			});			
+           
+            
+            //slide show
+            var Slider = function(){
+                this.imgs = slideFrame.find('div.image');
+                this.imgCount = (this.imgs.length) - 1; 
+                this.captions = this.imgs.find('p');
+                this.details = slideFrame.find('.details');
+               
+                                 // Init
+                this.init = function(){ 
+                    slideFrame
+                        //.width((setting.i_width +10) * (this.imgCount+1))
+                        .width(setting.width)
+                        .height(setting.height);
+                    this.imgs
+                        .width(setting.i_width)
+                        .height(setting.i_height);
+                    itemFrame.height(setting.height);
+                    this.imgs.find('img').height(setting.i_height);
+                    // captions
+                    this.captions.width(setting.i_width + 'px');
+                    //details
+                    this.details
+                        .width(setting.i_width)
+                        .height(setting.height - setting.i_height-30);
+                    $('.slideBtn').css("width", setting.width +"px");
+                    
+                };
+            };
+               
+            var slider = new Slider();
+            slider.init();		
+            var auto;
+            var auto_flag;
+            // get number of item displaying 
+            var num = Math.floor(setting.width/(setting.i_width + setting.padding));
+            
+            
+            $(document).ready(function(e) {
+                //next button
+                $('.next').bind("click", function enableNext(){
+                    clearInterval(auto);
+                    $(".next").unbind("click");
+                    //clone
+                    var clone= $('.itemFrame').eq(num).clone();
+                    clone.insertBefore($('.itemFrame').eq(num));
+                    clone.css("position","absolute");
+                  
+                    $('.itemFrame:first').animate({
+                        marginLeft: '-='+ (setting.i_width+ setting.padding)
+                      }, setting.tranSpeed, function() {
+                            $('.itemFrame:first').css("marginLeft",setting.padding);
+                            $('.itemFrame:first').insertAfter($('.itemFrame:last'));
+                            $(".next").bind("click",enableNext);
+                            clone.remove();
+                    });
+                    if (auto_flag == 1) {
+                        auto = setInterval(auto_change, 5000);
+                    }
+                    
+                });
+                //prev button
+                $('.prev').bind("click", function enablePrev(){
+                    $(".prev").unbind("click");
+                    //clone end slide
+                    var clone_end= $('.itemFrame').eq(num-1).clone();
+                    clone_end.insertBefore($('.itemFrame').eq(num-1));
+                    clone_end.css("position","absolute");
+                    //clone before slide
+                    var clone_start = $('.itemFrame:last').clone();
+                    clone_start.css("margin-left", -(setting.i_width+ setting.padding));
+                    clone_start.insertBefore($('.itemFrame:first'));
+                    //debugger;
+                    $('.itemFrame:first').animate({
+                        marginLeft: '+='+ (setting.i_width+ setting.padding*2)
+                      }, setting.tranSpeed, function() {
+                         
+                           // $('.itemFrame:first').css("marginLeft",setting.padding);
+                            $('.itemFrame:last').insertBefore($('.itemFrame:first')); 
+                            $(".prev").bind("click",enablePrev);
+                            clone_end.remove();
+                            clone_start.remove();
+                    });
+                });
+
+                //auto button
+                function auto_change(){
+                    $('.next').click();
+                }
+                $(".play").click(
+                    function() {
+                        auto_flag = 1;
+                        auto = setInterval(auto_change, 3000);
+                        $(this).css("background-color","black");
+                        $(this).attr("disabled",true);
+                        $(".stop").css("background-color","");
+                      
+                    }
+                );
+                $(".stop").click(
+                    function () {
+                        clearInterval(auto);
+                        $(".play").css("background-color","");
+                        $(this).css("background-color","black");
+                        $(".play").removeAttr("disabled");
+                    }
+                );
+                $(".stop").click();
+            });
+            
+
+        })
+
+    }
+
+})(jQuery);
---
+++ Javascript/SlideShow/Fade_plugin/script.js
@@ -0,0 +1,159 @@
+(function($){
+    $.fn.slideShow = function(options) {
+        var setting = {
+            'width': 900,
+            'height': 600,
+            'bullets': true,
+            'tranSpeed':1000
+        };
+
+        this.each(function() {
+            if(options) {
+                $.extend(setting, options);
+            }
+            // add class
+            $(this).wrapAll('<div class="slideShow" />');
+           
+            var contain = $(this).find('.slideShow');
+            
+            contain.find('li').wrapAll('<div class="frame" />');
+            var frame = contain.find('.frame');
+
+            // add button next, prev
+            frame.append('<a href="#" class="prev">&lt;</a> <a href="#" class="next">&gt;</a>');
+            $('<div style="margin: 0 auto; width:'+ setting.width + 'px"><button class="play">PLAY</button> <button class="stop">STOP</button></div>')
+            .insertBefore(frame);
+            // add list number
+            contain.append('<div class="num_list" />');
+            var num_list = contain.find('.num_list');
+
+            frame.find('img').each(function(i){
+                
+				i += 1; 
+				if (setting.bullets === true) { 
+                    num_list.append('<a href="#">'+ i +'</a>'); 
+				}
+
+				// Caption
+				var title = $(this).attr('title');
+				$(this).wrapAll('<div class="image" />');
+				if (title !== undefined) {
+						$(this).attr('title', '');
+						$(this).after('<p>' + title + '</p>');
+				}
+			});			
+            
+            
+            //slide show
+            var Slider = function(){
+                this.imgs = frame.find('div.image');
+                this.imgCount = (this.imgs.length) - 1; 
+                this.Prev = frame.find('a.prev');
+                this.Next = frame.find('a.next');
+                this.bullets = contain.find('.num_list a');
+                this.captions = this.imgs.find('p');
+                var index=0;
+                var auto_flag =0;
+                this.switchImg = function(index){ // change image
+                        this.imgs
+                            .removeClass('curr')
+                            .fadeOut(setting.fadeSpeed)
+                            .eq(index)
+                            .fadeIn(setting.fadeSpeed)
+                            .addClass('curr');
+                        this.bullets
+                            .removeClass('curr')
+                            .eq(index)
+                            .addClass('curr');
+
+                };
+                this.changeIndex = function(newval){ // change index
+                        index=newval;
+                };
+                this.next = function(){
+                        if (index < this.imgCount) {
+                           index = (index + 1); 
+                        } else {
+                           index = 0;
+                        }
+                        this.switchImg(index);
+                };
+        
+                this.prev = function(){
+                        if (index > 0) {
+                            index = (index - 1); 
+                        } else {
+                            index = this.imgCount;
+                        }
+                        this.switchImg(index);
+                };	
+
+                 // Init
+                this.init = function(){ 
+                    frame
+                        .width(setting.width)
+                        .height(setting.height);
+                    
+                    this.imgs.hide().first().addClass('curr').show(); 
+                    this.bullets.first().addClass('curr');
+                    
+                    num_list.css('width', setting.width);
+                   
+                    this.captions // captions
+                        .width(setting.width + 'px');
+                };
+            };
+               
+            var slider = new Slider();
+            console.log(setting);
+            slider.init();		
+                
+			frame.hover(function(){ 
+				slider.Next.stop(true, true).fadeToggle();
+				slider.Prev.stop(true, true).fadeToggle();
+			});
+			slider.Next.click(function(){ 
+                clearInterval(auto);
+                slider.next(); 
+                if (auto_flag == 1) {
+                    auto = setInterval(auto_change, 5000);
+                }
+			});
+			slider.Prev.click(function(){ 
+				slider.prev();
+			});
+			slider.bullets.click(function(){  
+                
+                slider.switchImg($(this).index());
+                slider.changeIndex($(this).index());
+            });
+
+            //autoplay 
+            var auto;
+            function auto_change(){
+                slider.Next.click();
+            }
+            $(".play").click(
+                function() {
+                    auto_flag = 1;
+                    auto = setInterval(auto_change, 3000);
+                    $(this).css("background-color","gray");
+                    $(this).attr("disabled",true);
+                    $(".stop").css("background-color","");
+                  
+                }
+            );
+            $(".stop").click(
+                function () {
+                    clearInterval(auto);
+                    $(".play").css("background-color","");
+                    $(this).css("background-color","gray");
+                    $(".play").removeAttr("disabled");
+                }
+            );
+            $(".stop").click();
+        })
+
+    }
+
+})(jQuery);
