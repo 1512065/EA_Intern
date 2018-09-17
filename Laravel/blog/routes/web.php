70ca120c93e8e060e774bb0f8aaa4de6d8c94638
1512065 <1512065>
2018-09-17 16:54:18 +0700

    simple demo

--- Laravel/blog/routes/web.php
+++ Laravel/blog/routes/web.php
@@ -10,11 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    //echo 'hello world';
-    return view('welcome');
-});
 
 Route::get('/uploadfile', 'UploadfileController@index');
 Route::post('/uploadfile', 'UploadfileController@upload');
@@ -22,3 +20,45 @@ Route::get('/main', 'MainController@index');
 Route::post('/main/checklogin', 'MainController@checklogin');
 Route::get('main/successlogin', 'MainController@successlogin');
 Route::get('main/logout', 'MainController@logout');
+
+Route::get('user/{id}', function ($id) {
+    return 'user: '.$id;
+})->where('id','[0-9]+');
+
+////////////////////////////////////////////
+Route::get('/view', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'task_name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/view')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->task_name = $request->task_name;
+    $task->save();
+
+    return redirect('/view');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/view');
+});
